b0f889b79cdf9d7605a60eb760003558a99de38a
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-06 16:10:06 +0530

    created

---
+++ number_changing_pyramid.java
@@ -0,0 +1,18 @@
+public class number_changing_pyramid {
+    
+    public static void main(String[] args){
+
+        int n =4;
+        int nol = 1;
+        int num =1;
+
+        while(nol<=4){
+
+            for(int i=1;i <= nol; i++ , num++){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            nol++;
+        }
+    }
+}
