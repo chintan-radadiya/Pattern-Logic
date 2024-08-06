cab06393b51383015210ccd003f999097cda2e86
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-06 16:03:49 +0530

    increasing_number_reverse_triangle

---
+++ increasing_number_reverse_triangle.java
@@ -0,0 +1,16 @@
+public class increasing_number_reverse_triangle{
+    public static void main(String[] args){
+
+        int n=4;
+        // int nol =4;
+
+        while(n>0){
+
+            for(int i=1;i<=n; i++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            n--;
+        }
+    }
+}
